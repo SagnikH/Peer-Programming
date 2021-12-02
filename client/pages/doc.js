1d37a2f609eff8acc226316ddbf9b4ab08dc0456
Sattam <officialbsattam@gmail.com>
2021-12-02 20:29:53 +0530

    working on displaying leetcode question

---
+++ client/pages/doc.js
@@ -0,0 +1,48 @@
+import ReactHtmlParser, { processNodes, convertNodeToElement, htmlparser2 } from 'react-html-parser';
+import styles from '../styles/Doc.module.css';
+
+const doc = () => {
+    const lc = '<div class="content__u3I1 question-content__JfgR"><div><p>Given the <code>root</code> of a binary search tree, return <em>a <strong>balanced</strong> binary search tree with the same node values</em>. If there is more than one answer, return <strong>any of them</strong>.</p>\n' +
+    '\n' +
+    '<p>A binary search tree is <strong>balanced</strong> if the depth of the two subtrees of every node never differs by more than <code>1</code>.</p>\n' +
+    '\n' +
+    '<p>&nbsp;</p>\n' +
+    '<p><strong>Example 1:</strong></p>\n' +
+    '<img alt="" src="https://assets.leetcode.com/uploads/2021/08/10/balance1-tree.jpg" width="500" />\n' +
+    '<pre><strong>Input:</strong> root = [1,null,2,null,3,null,4,null,null]\n' +
+    '<strong>Output:</strong> [2,1,3,null,null,null,4]\n' +
+    '<b>Explanation:</b> This is not the only correct answer, [3,1,4,null,2] is also correct.\n' +
+    '</pre>\n' +
+    '\n' +
+    '<p><strong>Example 2:</strong></p>\n' +
+    '<img alt="" src="https://assets.leetcode.com/uploads/2021/08/10/balanced2-tree.jpg" width="500" />\n' +
+    '<pre><strong>Input:</strong> root = [2,1,3]\n' +
+    '<strong>Output:</strong> [2,1,3]\n' +
+    '</pre>\n' +
+    '\n' +
+    '<p>&nbsp;</p>\n' +
+    '<p><strong>Constraints:</strong></p>\n' +
+    '\n' +
+    '<ul>\n' +
+    '\t<li>The number of nodes in the tree is in the range <code>[1, 10<sup>4</sup>]</code>.</li>\n' +
+    '\t<li><code>1 &lt;= Node.val &lt;= 10<sup>5</sup></code></li>\n' +
+    '</ul>\n' +
+    '</div></div>';
+    const reactLc = ReactHtmlParser(lc);
+    console.log(reactLc[0].props.children);
+    return ( 
+        <div className={styles.question}>
+            <div
+            dangerouslySetInnerHTML={{__html: lc}}>
+            </div>
+        </div>
+        
+     );
+}
+ 
+export default doc;
+
+/**
+ * 
+
+*/
