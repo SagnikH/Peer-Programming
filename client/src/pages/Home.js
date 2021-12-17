d29cf758544c95a55fabc9d27ddccc8ed6e50a17
Sattam <officialbsattam@gmail.com>
2021-12-18 00:26:49 +0530

    fixed coworking svg

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -5,8 +5,9 @@ import axios from "axios";
 import Cookies from "js-cookie";
 import { addUser, removeUser } from "../store/userSlice";
 import { addToken, removeToken } from "../store/authTokenSlice";
-import styles from "../styles/home.css";
+import "../styles/home.css";
 import { Button, Form } from "react-bootstrap";
+import coWorking from '../assets/co-working.svg';
 
 const Home = () => {
 	const dispatch = useDispatch();
@@ -64,31 +65,30 @@ const Home = () => {
 
 	return (
 		<>
-			<div className={styles.homepage}>
-				<div className="mt-3" height="500" width="500">
-					<img src="/co-working.svg" height="500" width="500" />
-				</div>
-				<div className={styles.contents}>
-					<div className={styles.subHeading}>Think and Code Together, in</div>
-					<div className={styles.heading}>Apes Collab</div>
-					<div className={styles.description}>
+			<div className='homepage'>
+				<img src={coWorking} alt="" height={500} width={500} />
+			
+				<div className='contents'>
+					<div className='subHeading'>Think and Code Together, in</div>
+					<div className='heading'>Apes Collab</div>
+					<div className='description'>
 						Turn your best ideas into reality, by coding and building together
 						with your peers and friends, in real-time. So what are you waiting
 						for? Let's dive in.
 					</div>
 					<div className="d-flex w-100 justify-content-around">
-						<Button className={styles.formButton} onClick={createLinkHandler}>
+						<Button className='formButton' onClick={createLinkHandler}>
 							Create Link
 						</Button>
 
 						<Form className="d-flex">
 							<Form.Control
-								className={styles.formInput}
-								type="text"
+								className='formInput'
+								type="text" 
 								placeholder="Enter link"
 							/>
 							<Button
-								className={styles.formButton}
+								className='formButton'
 								type="submit"
 								onClick={joinLinkHandler}
 							>
