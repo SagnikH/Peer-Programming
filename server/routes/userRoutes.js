c8267e3764d34596a4dc6dd7d64d7b650326eb62
Ronak Sanpui <ronaksanpui@gmail.com>
2022-01-02 00:09:29 +0530

    ready for deploy

--- server/routes/userRoutes.js
+++
@@ -1,8 +0,0 @@
-const { userHandler } = require("../controllers/userHandler");
-const { verifyJWT } = require("../middlewares/authMiddleware");
-
-const router = require("express").Router();
-
-router.get("/", verifyJWT, userHandler);
-
-module.exports = router;
